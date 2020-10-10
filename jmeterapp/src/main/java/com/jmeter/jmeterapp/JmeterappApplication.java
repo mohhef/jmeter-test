5ad0d8b1f0377626de0f52f3ae9079594bdb73ac
mohhef <mohhef@rocketmail.com>
2020-10-10 15:54:17 -0400

    project setup

---
+++ jmeterapp/src/main/java/com/jmeter/jmeterapp/JmeterappApplication.java
@@ -0,0 +1,13 @@
+package com.jmeter.jmeterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmeterappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmeterappApplication.class, args);
+	}
+
+}
---
+++ jmeterapp/src/test/java/com/jmeter/jmeterapp/JmeterappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmeter.jmeterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmeterappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
