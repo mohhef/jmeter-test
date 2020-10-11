d0860257848201b51ad85c0a1541aa682de871c0
mohhef <mohhef@rocketmail.com>
2020-10-11 16:20:05 -0400

    test

--- runjob.sh
+++ runjob.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Runs tests from JMX file, creates results file and reports dashboard
-apache-jmeter-3.3/bin/jmeter -n -t tests/$2.jmx -l results/$2Results.jtl -e -o $2-reports
+apache-jmeter-3.3/bin/jmeter -n -t tests/$1.jmx -l results/$1Results.jtl -e -o $1-reports
 
 # Print results file
-cat results/$2Results.jtl
+cat results/$1Results.jtl
 
 # Curl back to webhook step in Jenkins pipeline
 # Give pipeline the name of the test suite pod
